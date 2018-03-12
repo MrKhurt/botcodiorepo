34c4a0449ddaf4be50133e2a3d85d8df1ea99077
khurt <matteo.spimpolo@gmail.com>
2018-03-12 22:17:48 +0100

    prova3
    prova3

--- execute.php
+++ execute.php
@@ -1,6 +1,6 @@
 <?php
 // includo il file imagessearch.php per la ricerca immagini da google images
-include 'imagessearch.php';
+include 'searchimages/ImageSearch.php';
 // recupero il contenuto inviato da Telegram
 $content = file_get_contents("php://input");
 // converto il contenuto da JSON ad array PHP
@@ -28,27 +28,14 @@ $response = '';
 
 if(strpos($text, 'culo') !== false)
 {
-    $url = "http://images.google.it/images?as_q=##query##&hl=it&imgtbs=z&btnG=Cerca+con+Google&as_epq=&as_oq=&as_eq=&imgtype=&imgsz=m&imgw=&imgh=&imgar=&as_filetype=&imgc=&as_sitesearch=&as_rights=&safe=images&as_st=y";
-    $web_page = file_get_contents( str_replace("##query##",urlencode('hot ass girl'), $url ));
- 
-    $tieni = stristr($web_page,"dyn.setResults(");
-    $tieni = str_replace( "dyn.setResults(","", str_replace(stristr($tieni,");"),"",$tieni) );
-    $tieni = str_replace("[]","",$tieni);
-    $m = preg_split("/[\[\]]/",$tieni);
-    $x = array();
-    for($i=0;$i<count($m);$i++) {
-        $m[$i] = str_replace("/imgres?imgurl\\x3d","",$m[$i]);
-        $m[$i] = str_replace(stristr($m[$i],"\\x26imgrefurl"),"",$m[$i]);
-        $m[$i] = preg_replace("/^\"/i","",$m[$i]);
-        $m[$i] = preg_replace("/^,/i","",$m[$i]);
-        if ($m[$i]!="") array_push($x,$m[$i]);
-    }
-    $images = $x;
+    ImageSearch::config()->apiKey('016485041220097449938');
+    ImageSearch::config()->cx('ox4wv57es20');
+    $images = ImageSearch::search('hot ass girl'); // returns array of results
     //$images = getGoogleImg('hot ass girl');
     $min=1;
     $max=8;
     $randomNumber = rand($min, $max);
-    $response = $images[$randomNumber] . ' culotette';
+    $response = $images[$randomNumber];
 }
 if(strpos($text, 'nonno fiorucci') !== false)
 {
