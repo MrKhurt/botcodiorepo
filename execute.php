fa6db201cea86875c1920e939e6ee74c0bfa566d
MrKhurt <37268117+MrKhurt@users.noreply.github.com>
2018-03-12 22:50:54 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -35,10 +35,10 @@ if(strpos($text, 'culo') !== false)
     ImageSearch::config()->apiKey('AIzaSyChwnAsJAEgWgbj06tNHIa54KgC95n5O_Y');
     ImageSearch::config()->cx('016485041220097449938:ox4wv57es20');
     $images = ImageSearch::search('hot ass girl', ['imgSize' => 'large']);
-    $min=1;
-    $max=8;
-    $randomNumber = rand($min, $max);
-    $response = $images[1] . ' seh ciao';
+    //$min=1;
+    //$max=8;
+    //$randomNumber = rand($min, $max);
+    $response = $images["items"]["link"] . ' seh ciao';
 }
 if(strpos($text, 'nonno fiorucci') !== false)
 {
