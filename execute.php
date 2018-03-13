541a1cee1b6376a5d171ce738e03e243d98541d0
khurt <matteo.spimpolo@gmail.com>
2018-03-14 00:01:29 +0100

    fix
    fix

--- execute.php
+++ execute.php
@@ -31,38 +31,7 @@ $response = '';
 // INIZIO
 if(strpos($text, 'comandi') !== false)
 {
-    $response = urlencode("Comandi BotCodio:
-        culo
-        figa 
-        fica
-        gnagna
-        gnocca
-        zozzerellona
-        zozza
-        buongiorno
-        tette
-        poppe
-        zinne
-        tettona
-        mammelle
-        capezzoli
-        capezzolo
-        ratajkowski
-        nargi
-        decker
-        nonno fiorucci
-        bek
-        beck
-        bestemmia
-        bestemmione
-        blasfemia
-        nonno
-        proverbio
-        poesia
-        saluta
-        chi?
-        insulta
-        offendi");
+    $response = 'Comandi BotCodio:'.chr(10).'culo '.chr(10).'figa '.chr(10).'fica '.chr(10).'gnagna '.chr(10).'gnocca '.chr(10).'zozzerellona '.chr(10).'zozza '.chr(10).'buongiorno '.chr(10).'tette '.chr(10).'poppe '.chr(10).'zinne '.chr(10).'tettona '.chr(10).'mammelle '.chr(10).'capezzoli '.chr(10).'capezzolo '.chr(10).'ratajkowski '.chr(10).'nargi '.chr(10).'decker '.chr(10).'nonno fiorucci '.chr(10).'bek '.chr(10).'beck '.chr(10).'bestemmia '.chr(10).'bestemmione '.chr(10).'blasfemia '.chr(10).'nonno '.chr(10).'proverbio '.chr(10).'poesia '.chr(10).'saluta '.chr(10).'chi? '.chr(10).'insulta '.chr(10).'offendi';
 }
 if(strpos($text, 'culo') !== false)
 {
