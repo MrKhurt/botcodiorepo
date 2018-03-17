5f2c3e5439335593f810e34a381badbf30c06a17
khurt <matteo.spimpolo@gmail.com>
2018-03-17 20:27:31 +0100

    D
    D

--- execute.php
+++ execute.php
@@ -306,7 +306,7 @@ else if(strpos($text, 'buongiorno') !== false)
     $resto = $buongiorno03;
   else if($randomNumber == 4)
     $resto = $buongiorno04;
-    $response = $response . '  ' . $resto . ' ' . date('d');
+    $response = $response . '  ' . $resto . ' ' . date('D');
     // oggi è sabato o domenica?
     if(date('D', $timestamp) === 'Sat' || date('D', $timestamp) === 'Sun') 
       $response = $response . '  ' . 'Com\'è andata ieri sera? Caccagialla?';
