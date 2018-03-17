339cf1030ad52b48604e6c856672e945f3bccbbf
khurt <matteo.spimpolo@gmail.com>
2018-03-17 21:29:03 +0100

    ddd
    ddd

--- execute.php
+++ execute.php
@@ -69,7 +69,6 @@ else if(substr($text, 0, 11) === 'cerca foto ')
     $resto = $arr[1];
     // termina con ' gif'? allora cerco una gif
     $length = strlen($resto);
-    $response = $response . $resto;
     if(length !== 0)
     {
       if (substr($resto, -4) === ' gif')
@@ -84,7 +83,7 @@ else if(substr($text, 0, 11) === 'cerca foto ')
       $min=1;
       $max=10;
       $randomNumber = rand($min, $max);
-      $response = $response . $images["items"][$randomNumber]["link"];
+      $response = $images["items"][$randomNumber]["link"];
     }
 }
 
