49325f330c609b914215d6c09fdda926acbb6024
MrKhurt <37268117+MrKhurt@users.noreply.github.com>
2018-03-20 09:17:51 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -774,7 +774,7 @@ else if(strpos($text, 'piove') !== false || strpos($text, 'pioggia') !== false)
 else if(strpos($text, 'meteo') !== false || 
         strpos($text, 'che tempo fa') !== false)
 {
-    $response = https://www.ilmeteo.it/meteo/Verona;
+    $response = 'https://www.ilmeteo.it/meteo/Verona';
 }
 
 // BUONANOTTE
